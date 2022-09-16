db054f0d10cce7edaedb95b3af4a349feaa89f40
AlenaD-B27 <lenador.18@gmail.com>
2022-09-16 17:59:54 -0500

    Project is created, task is posted, gitignore file is created.

---
+++ src/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+     /*
+1. Accept the invitation;
+2. Clone the project to your IntelliJ;
+3. Checkout to origin develop branch, it will create your local develop one;
+4. Create your branch from local develop, name of the branch should be your name. Ex: my branch will be "alena";
+5. Create the package, name it name_goes_camping (use your name);
+6. Create the class Tourist. Commit. All the other commits and pushes can be done whenever you feel it's time to "save".
+7. Create variables: String name and String [] belongings. Initialize variables: create Constructor with 1 argument(name), encapsulate belongings. Requirement: 1 tourist must take exactly 3 items.
+8. Create toString method. Method should return "name goes camping and has [item, item, item]".
+9. Create methods:
+- setUpTent(){Requirement: if tourist has tent it should print "Tent is ready, tonight name is sleeping in a tent.", if not => "name doesn't have a tent, tonight name is sleeping under the stars."};
+- makeFire(){Requirement: if tourist has matches, lighter or flint => it should print "The fire is ready." Otherwise => "name has no items to make the fire."};
+- playMusic(){if tourist has guitar, speaker or radio it should print "The music is fun." Otherwise: "name has no music so name is singing, the wolves think it's their brother"}.
+10. In your package create the class Camping.
+11. In class Camping create the main method and import Scanner. Create an object Tourist tourist1. Complication: set belongings using the Scanner.  Test: toString, setUpTent and makeFire methods.
+9. Push all the changes to GitHub.
+10. Create PR from YOUR branch to develop. If you request to pull it to master I will have to reject it, so, please, pay attention :)
+     */
+}
