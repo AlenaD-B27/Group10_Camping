4df87401fd82a2274111632801b4a1001f7b4129
AlenaD-B27 <lenador.18@gmail.com>
2022-09-19 17:01:43 -0500

    Working on Tourist.java: variables, constructor, encapsulation, toString are done

--- src/alena_goes_camping/Tourist.java
+++ src/alena_goes_camping/Tourist.java
@@ -1,4 +1,25 @@
 package alena_goes_camping;
 
+import java.util.Arrays;
+
 public class Tourist {
+
+    String name;
+    private String [] belongings = new String [3];
+
+    public Tourist(String name) {
+        this.name = name;
+    }
+
+    public String[] getBelongings() {
+        return belongings;
+    }
+
+    public void setBelongings(String[] belongings) {
+        this.belongings = belongings;
+    }
+
+    public String toString(){
+        return name + " goes camping and has " + Arrays.toString(belongings) + " .";
+    }
 }
